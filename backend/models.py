418d05d09223f25ad68a5399fd36e695e0a147ef
Shreyas Agarwal <shreyas.agarwal2701@gmail.com>
2025-04-16 15:55:40 +0530

    updated models.py

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
-class Item():
-    name: int
+class Item(BaseModel):
+    name: str
     description: str
-
+    # fixed the item class
 class User(BaseModel):
     username: str
     bio: str
